a201fc268839455d30bddfdf94958404fa12a9db
Kiril Yakymchuk <stbestich@gmail.com>
2024-03-31 17:10:19 +0300

    Added new endpoint

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+import numpy
 
 router = APIRouter()
 
@@ -6,3 +8,14 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/numpy')
+def multiply_matrix():
+    A = numpy.random.random_integers(0, 10, (10, 10))
+    B = numpy.random.random_integers(0, 10, (10, 10))
+    product = A.dot(B);
+    return JSONResponse(content={
+        'matrix_A': A.tolist(),
+        'matrix_B': B.tolist(),
+        'product': product.tolist(),
+    })
